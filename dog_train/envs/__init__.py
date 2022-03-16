fced5d3f457b050b70905ffa34363c9e09bf834d
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2022-03-16 11:14:49 +0700

    Update __init__.py

--- dog_train/envs/__init__.py
+++ dog_train/envs/__init__.py
@@ -1,4 +1,16 @@
-from gym.envs.registration import register
+from gym.envs.registration import (
+    registry,
+    register,
+    make,
+    spec,
+    load_env_plugins as _load_env_plugins,
+)
+
+# Hook to load plugins from entry points
+_load_env_plugins()
+
+# Classic
+# ----------------------------------------
 
 register(
     id='dog-v0',
