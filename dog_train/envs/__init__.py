18279cedf3a8b497fc9bcbab61f1a9d00a56213c
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2022-03-16 11:02:36 +0700

    Update __init__.py

--- dog_train/envs/__init__.py
+++ dog_train/envs/__init__.py
@@ -2,10 +2,10 @@ from gym.envs.registration import register
 
 register(
     id='dog-v0',
-    entry_point='dog_train.envs:DogEnv',
+    entry_point='dog_train.envs.basic_env:DogEnv',
 )
 
 register(
     id='dog-v2',
-    entry_point='dog_train.envs:dogEnv2',
+    entry_point='dog_train.envs.basic_env:dogEnv2',
 )
